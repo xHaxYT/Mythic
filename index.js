49fbc3bc7815feaa792be17b0ee31d1a1398645c
Glitch (skinny-broad-skink) <none>
2020-04-11 14:52:43 +0000

    🚩⚽️ Checkpoint ./index.js:14185214/3026 ./colours.json:14185214/527 ./util/eventHandler.js:14185214/348 ./commands/help.js:14185214/2106 ./events/warn.js:14185214/87 ./events/error.js:14185214/96 ./events/ready.js:14185214/516

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-const http = require("http")
-const express = require("express")
-const app = express();
-app.get ("/", (request, response) => { 
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {})
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const botconfig = require("./botconfig.json");
 const colours = require("./colours.json");
-const bot = new Discord.Client({ disableEveryone: true });
 
-bot.on("ready", async () => {
-  console.log("Bot is online");
-  bot.user.setActivity("Testing", { type: "STREAMING" });
+const bot = new Discord.Client({disableEveryone: true});
+
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
 });
+
+//console chatter
+let y = process.openStdin()
+y.addListener("data", res => {
+    let x = res.toString().trim().split(/ +/g)
+    bot.channels.get("555039958121971736").send(x.join(" "));
+});
+
+
 
 bot.on("message", async message => {
-  if (message.author.bot || message.channel.type === "dm") return;
+    if(message.author.bot || message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  //test command
-  if (cmd === "hello") {
-    return message.channel.send("Hello");
-  }
-  //clan intro
-  if (cmd === "clan-intro") {
-    return message.author.send(
-      "Here is Mythic Official Clan intro: https://drive.google.com/file/d/1Ay_JkN_mbfsczRtMLu3IaNW9PjZK2LR2/view"
-    );
-  }
-  //help command
-  if (cmd === "help") {
-    let sEmbed = new Discord.RichEmbed()
-      .setColor(colours.lime)
-      .setTitle("List of Commands")
-      .addField("**clan-intro**", "Official Clan Inro")
-      .addField("**help**", "Send you list of commands");
-    return message.author.send({ embed: sEmbed });
-  }
-});
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0].toLowerCase();
+    let args = messageArray.slice(1);
+
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
 
 bot.login(botconfig.token);
