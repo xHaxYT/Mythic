beee23e71f6de777578033f8c72690eddb6b789a
Glitch (skinny-broad-skink) <none>
2020-04-13 15:15:11 +0000

    🚥🎺 Checkpoint ./commands/embed.js:14185214/3742

--- commands/embed.js
+++ commands/embed.js
@@ -4,18 +4,25 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You can not use this command!")
     
-    let argsresult;
-    let mChannel = message.mentions.channels.first()
+     message.delete()
+    // mentioned or grabbed user
+    let author = message.mentions.members.first() || message.guild.members.get(args[0])
+    if(!author) return message.channel.send("Please provide a valid user").then(m => m.delete(15000))
 
-    message.delete()
-    if(mChannel) {
-        argsresult = args.slice(1).join(" ")
-        mChannel.send(argsresult)
-    } else {
-        argsresult = args.join(" ")
-        message.channel.send(argsresult)
-    }
+    // reasoning definition
+    let reason = args.slice(1).join(" ")
+    if(!reason) return message.channel.send(`Please provide a reason for reporting **${author.user.tag}**`).then(m => m.delete(15000))
 
+    // grab reports channel
+    let sChannel = message.mentions.channels.first()
+
+    // send to reports channel and add tick or cross
+
+    let ssEmbed = new Discord.RichEmbed()
+    .setAuthor(`${author.user.tag}`, author.user.displayAvatarURL)
+    .setTimestamp()
+    .setFooter(`MYTHIC Clan`, bot.user.displayAvatarURL);
+    sChannel.send({embed: ssEmbed})
 }
 
 
