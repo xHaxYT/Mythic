317086666ede0298f241557919470f61b0ab402e
Glitch (mythic-b) <none>
2020-04-29 15:49:06 +0000

    😘😋 Checkpoint ./commands/emoji.js:14185214/158

--- commands/emoji.js
+++ commands/emoji.js
@@ -5,11 +5,8 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete()
 
-    let name = args.slice(0).join(" ")
-    if(!name) return message.channel.send("Input valid emoji name!").then(m => m.delete(15000))
-
     // reasoning definition
-    let id = args.slice(1).join(" ")
+    let id = args.slice(0)
     if(!id) return message.channel.send(`Input valid emoji ID!`).then(m => m.delete(15000))
 
     // send to reports channel and add tick or cross
@@ -18,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor(colours.red)
     .setAuthor(`${message.author.tag}`, message.auhtor.displayAvatarURL)
     .setTimestamp()
-    .addField(`<:${name}:${id}`, true)
+    .addField(`<a:${id}>`, true)
     .setFooter(`MYTHIC Clan`, bot.user.displayAvatarURL);
     message.channel.send(eEmbed)
 
